7b302e422fd225323c7c741f35b0af0e6e9f7c67
Jeevan <jeevanjnayak@gmail.com>
2022-03-06 18:03:11 +0530

    Added : analysing mood using exception handling

---
+++ main/java/MoodAnalysisException.java
@@ -0,0 +1,19 @@
+import java.io.Serial;
+
+public class MoodAnalysisException extends Exception {
+
+    @Serial
+    private static final long serialVersionUID = 1L;
+
+    enum ExceptionType {ENTERED_NULL,ENTERED_EMPTY}
+    ExceptionType type;
+
+    public MoodAnalysisException(String message) {
+        super(message);
+    }
+
+    public MoodAnalysisException(ExceptionType type,String message) {
+        super(message);
+        this.type=type;
+    }
+}
